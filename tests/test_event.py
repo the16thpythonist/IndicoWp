05492160a92c736a5f4bda0743d2fd2c8eae1283
the16thpythonist <jonseb1998@gmail.com>
2018-02-10 16:57:16 +0100

    test fixture for basic event test

--- tests/test_event.py
+++ tests/test_event.py
@@ -6,4 +6,13 @@ import pytest
 @pytest.fixture()
 def event(request):
     # Creating a normal EventCreator
-    pass
+    event_creator = EventCreator(1, 'Max', 'Mustermann', 'KIT')
+    # Creating default time
+    event_time = EventTime('02:00:00', '2020-01-01')
+    # Creating default description
+    event_description = EventDescription('test', 'testing', 'seminar')
+    # Creating default Location
+    event_location = EventLocation('KIT', 'Karlsruhe')
+
+    event = Event(event_description, event_location, event_time, event_creator)
+    return event
