969fe38a236f4ab183278e63fbb78e844b7f31d0
the16thpythonist <jonseb1998@gmail.com>
2018-02-10 16:43:46 +0100

    Began working on pytest test files

---
+++ tests/test_event.py
@@ -0,0 +1,9 @@
+from IndicoWp.event import EventCreator, EventTime, EventLocation, EventDescription, Event
+
+import pytest
+
+
+@pytest.fixture()
+def event(request):
+    # Creating a normal EventCreator
+    pass
