4ff9c7a7cc3ee36da1de8e3c12325d4f3d20f96c
the16thpythonist <jonseb1998@gmail.com>
2018-02-11 08:57:15 +0100

    LoggingController: Log file name creation

--- config.py
+++ config.py
@@ -20,18 +20,28 @@ class LoggingController:
     :ivar log_name: The name of the log file for the current day
     :ivar path: The pathlib.Path object to the folder in which the logs are located
     """
-    DATE_FORMAT = '%Y-%m-%d'
+    DATE_FORMAT = '%Y_%m_%d'
     DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+    BASE_NAME = 'indico'
 
     def __init__(self):
 
         # The name of the folder which will contain the log files
         # TODO: Make this a part of the config.ini file
         self.logging_folder = 'logging'
-        self.log_name = 'test.log'
+        self.log_name = self._create_log_name()
 
         # The path object pointing to the folder with the logs
-        self.path = pathlib.Path(PATH) / self.logging_folder / ''
+        self.path = pathlib.Path(PATH) / self.logging_folder / self.log_name
+
+    @property
+    def path_string(self):
+        """
+        Returns the string path to the currently handled log file
+        :return:
+        """
+        return str(self.path)
 
     def init(self):
         """
@@ -86,15 +96,43 @@ class LoggingController:
         with self.path.open(mode='a') as file:
             file.write(line_string)
 
+    def _create_log_name(self):
+        """
+        Creates the log name based on the BASE_NAME class variable and the current date.
+
+        :return: The string name for the log file
+        """
+        # Creating the date string from the current date time object
+        date_string = self._current_date_string()
+
+        log_name = '{}_{}'.format(
+            self.BASE_NAME,
+            date_string
+        )
+
+        return log_name
+
     def _current_datetime_string(self):
         """
         Creates a datetime object for the current time of the method execution and formats the string based on the
-        format, that is given as static field of the logging controller.
+        format, that is given as the DATETIME_FORMAT class variable
 
         :return: The string of the current date & time
         """
         datetime_object = datetime.datetime.now()
         datetime_string = datetime_object.strftime(self.DATETIME_FORMAT)
+
+        return datetime_string
+
+    def _current_date_string(self):
+        """
+        Creates a datetime object for the current moment of the method being executed and formats the string based on
+        the format, that is given as the DATE_FORMAT class variable
+
+        :return: A string for the current date
+        """
+        datetime_object = datetime.datetime.now()
+        datetime_string = datetime_object.strftime(self.DATE_FORMAT)
 
         return datetime_string
 
